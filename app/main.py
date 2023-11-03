79f0442627a0d44df6f99e01ebef43236c84bb51
Angel Chávez <infoangelchavez@gmail.com>
2023-11-03 13:57:26 -0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -2,18 +2,27 @@ import socket
 
 HOST, PORT = "localhost", 4221
 
+routes = ['/']
+
+methods = {
+    200: b'HTTP/1.1 200 OK\r\n\r\n',
+    400: b'HTTP/1.1 400 Not Found\r\n\r\n'
+}
+
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen(1)
     print("Server in port:", PORT)
     conn, address = server.accept()
     print("Connected by:", address)
-    conn.recv(1024)
-    conn.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    data = conn.recv(1024).decode().splitlines()
+    http_status = data[0].split(' ')
+    if http_status[1] in routes:
+        conn.sendall(methods.get(200))
+    else:
+        conn.sendall(methods.get(400))
 
 
 if __name__ == "__main__":
