0d310c4e241c113775903c6b103dc3d0612b1fed
Angel Chávez <infoangelchavez@gmail.com>
2023-11-03 14:23:01 -0500

    pass 3nd stage

--- app/main.py
+++ app/main.py
@@ -2,16 +2,21 @@ import socket
 
 HOST, PORT = "localhost", 4221
 
-routes = ['/']
+ROUTES = ['/']
 
-methods = {
-    200: b'HTTP/1.1 200 OK\r\n\r\n',
-    400: b'HTTP/1.1 400 Not Found\r\n\r\n'
-}
+
+def get_response(code: int) -> bytes:
+    responses = {
+        200: 'OK',
+        400: 'Not Found',
+    }
+    return f"HTTP/1.1 {code} {responses.get(code)}\r\n\r\n".encode()
 
 
 def main():
-    server = socket.create_server((HOST, PORT), reuse_port=True)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(1)
     print("Server in port:", PORT)
     conn, address = server.accept()
     print("Connected by:", address)
@@ -20,11 +25,10 @@ def main():
     print(data[0])
 
     http_status = data[0].split(' ')
-    if http_status[1] in routes:
-        conn.sendall(methods.get(200))
+    if http_status[1] in ROUTES:
+        conn.sendall(get_response(200))
     else:
-        conn.sendall(methods.get(400))
-    conn.close()
+        conn.sendall(get_response(400))
 
 
 if __name__ == "__main__":
