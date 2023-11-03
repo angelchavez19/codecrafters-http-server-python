7c8d17f57caec858bafca4fe2942df733b3d2550
Angel Chávez <infoangelchavez@gmail.com>
2023-11-03 14:58:53 -0500

    pass 4nd stage

--- app/main.py
+++ app/main.py
@@ -5,16 +5,22 @@ HOST, PORT = "localhost", 4221
 ROUTES = ['/']
 
 
-def get_response(code: int) -> bytes:
+def get_response(code: int) -> str:
     responses = {
         200: 'OK',
         404: 'Not Found',
     }
-    return f"HTTP/1.1 {code} {responses.get(code)}\r\n\r\n".encode()
+    return f"HTTP/1.1 {code} {responses.get(code)}"
 
 
 def main():
-    server = socket.create_server(("localhost", 4221), reuse_port=True)
+    # Dev
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
+
+    # Deploy
+    # server = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server in port:", PORT)
     conn, address = server.accept()
     print("Connected by:", address)
@@ -24,9 +30,12 @@ def main():
 
     http_status = data[0].split(' ')
     if http_status[1][0] == '/' and http_status[1][-1] == '/':
-        conn.sendall(get_response(200))
+        conn.sendall(f"{get_response(200)}\r\n\r\n".encode())
     else:
-        conn.sendall(get_response(404))
+        param = http_status[1].split('/')[-1]
+        response = f"{get_response(200)}\r\nContent-Type: text/plain\r\n" + \
+            f"Content-Length: {len(param)}\r\n\r\n{param}"
+        conn.sendall(response.encode())
 
 
 if __name__ == "__main__":
