e90bf1285cda0cb9fb2a5baaa2cdc492227a3a5f
Angel Chávez <infoangelchavez@gmail.com>
2023-11-03 21:06:17 -0500

    pass 8nd stage

--- app/router/router.py
+++ app/router/router.py
@@ -5,14 +5,10 @@ from app.utils.response import get_response_not_found
 
 def router(request: Request, directory: str = None) -> Response:
     for path in urls:
-        print(1)
-        if path.method != request.path:
-            print(2)
+        if path.method != request.method:
             continue
-        print(3)
         if (path.start and request.path.startswith(path.path)) or \
                 (not path.start and request.path == path.path):
-            print(4)
             if path.directory:
                 return path.view(request=request, directory=directory)
             return path.view(request=request)
